a13bd5d6a0eefe184c989c14eac12d51979b6867
deanmcdonagh <36044271+deanmcdonagh@users.noreply.github.com>
2018-04-26 15:57:10 +0100

    Code Fixed 2

--- js/convert.js
+++ js/convert.js
@@ -2,15 +2,33 @@
   //Variables for functionality of the converter
   var measure = parseInt(document.getElementById("value1").value);
   var meters = measure * 0.3048;
+  var inches = measure * 2.54;
+  var yards = measure * 0.9144;
+  var miles = measure * 1.60934;
   var message = measure + ' feet converts to ' + meters + ' meters.';
+  var myAlert = 'please enter a value';
+
+//Convert Feet to Metres
+function feeToMetres() {
 
 
-
-function fToM() {
-
-console.flag="message"; 
- document.getElementbyId("resultsentence").innerHTML = message;
 }
+//Convert Inches to Centimetres
+function inchesToCentimetres(){
+	
+}
+//Convert Yards to Metres
+function YardsToMetres () {
+	
+}
+//Convert Miles to Kilometres
+function MilesToKilomtres () {
+	
+}
+
+console.log="message"; 
+document.getElementById("resultsentence").innerHTML = message; 
+ 
 
   
 
